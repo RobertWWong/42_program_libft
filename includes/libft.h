5cbab721a5abcfc1d10b14cb4bc85601232f3ac1
robertwwong <robertww@uci.edu>
2018-07-12 13:48:52 -0700

    made some mistakes during commit

--- includes/libft.h
+++ includes/libft.h
@@ -32,7 +32,7 @@ char		*ft_strcpy(char *dest, const char *src);
 char		*ft_strncpy(char *dest, const char *src, unsigned int n);
 char		*ft_strcat(char *dest, const char *src);
 char		*ft_strncat(char *dest, const char *src, int nb);
-size_t		ft_strlcat(char *dest, const char *src, size_t size);
+size_t		ft_strlcat(char *dest, const char *src, size_t dstsize);
 char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr(const char *s, int c);
 char		*ft_strstr(const char *haystack, const char *needle);
