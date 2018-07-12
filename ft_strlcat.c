7e22c7be384eb89bcb8a465c4b0b53a1ff6128a3
robertwwong <robertww@uci.edu>
2018-07-12 13:39:51 -0700

    Don't increment s in the block which would loop forever if s isn't incremented

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
 {
 	char			*d;
 	const char		*s;
@@ -21,11 +21,11 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 
 	d = dest;
 	s = src;
-	n = size;
+	n = dstsize;
 	while (n-- != 0 && *d != 0)
 		d++;
 	dlen = d - dest;
-	n = size - dlen;
+	n = dstsize - dlen;
 	if (n == 0)
 		return (dlen + ft_getlen(s));
 	while (*s != 0)
