716e3bbca69ddebb208af4628ee67b9b8d1ac121
robertwwong <robertww@uci.edu>
2018-07-14 12:40:07 -0700

    Part 1 + Part 2 function completed with no [failed] checks detected on 42FileChecker. Implementing linked list after error checking

--- includes/ft_strsplit.c
+++ includes/ft_strsplit.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-static	int	count_words(char const *s, char delimit)
+static	int	word_count(char const *s, char delimit)
 {
 	int words;
     int start_del;
@@ -33,13 +33,63 @@ static	int	count_words(char const *s, char delimit)
         else if (s[ctr] == delimit)
             start_del = 1;
         ctr++;
-
     }
     return (words);
 }
 
+static	int	next_pos(char *str, int pos, char delimit)
+{
+	while (str[pos] == delimit)
+		pos++;
+	return (pos);
+}
+
+static	int	word_len(char *str, int pos, char delimt)
+{
+	int len;
+
+	len = 0;
+	while (!(str[pos + len] == delimt || str[pos + len] == '\0'))
+		len++;
+	return (len);
+}
+
+static	int	copy_word(char **res, int ptrn, char *str, int pos, char delimit)
+{
+	int wlen;
+	int next_p;
+	int i;
+
+	next_p = next_pos(str, pos, delimit);
+	wlen = word_len(str, next_p, delimit);
+	res[ptrn] = (char *)malloc(sizeof(char*) * (wlen + 1));
+	i = 0;
+	while (i < wlen)
+	{
+		res[ptrn][i] = str[next_p + i];
+		i++;
+	}
+	res[ptrn][i] = '\0';
+	return (next_p + wlen);
+}
+
 char		**ft_strsplit(char const *s, char c)
 {
-	char		*ptr;
+	char	**res;
+	int		position;
+	int		i;
+	int		total_words;
 
+	i = 0;
+	position = 0;
+	total_words = word_count(s, c);
+	if (!(res = (char **)malloc(sizeof(char*) * (total_words + 1))))
+		return (NULL);
+	while (i < total_words)
+	{
+		position = copy_word(res, i, (char*)s, position, c);
+		i++;
+	}
+	res[i] = NULL;
+	return (res);
 }
